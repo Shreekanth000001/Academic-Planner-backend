93bcf8e0834509f2fba8aba9388ace3f8f4f7941
Shreekanth.k <Shreekanth.k000001@gmail.com>
2026-07-14 20:00:03 +0530

    Included stripe env variables

--- config.py
+++ config.py
@@ -7,6 +7,9 @@ class Settings(BaseSettings):
     SUPABASE_SERVICE_KEY:str
     GITHUB_PAT_TOKEN:str
     CLERK_WEBHOOK_SECRET:str
+    STRIPE_SECRET_KEY:str
+    STRIPE_PRICE_ID:str
+    STRIPE_WEBHOOK_SECRET:str
 
     model_config = SettingsConfigDict(env_file=".env",extra="ignore")
 
