916f858d7c90819ad344f8422ae99fa9577888b8
Shreekanth.k <Shreekanth.k000001@gmail.com>
2026-07-14 20:01:47 +0530

    Changed default credits of user from 2 to 200

--- models.py
+++ models.py
@@ -38,7 +38,7 @@ class User(Base):
     stripe_customer_id: Mapped[str | None] = mapped_column(String(255), unique=True, nullable=True)
     stripe_subscription_id: Mapped[str | None] = mapped_column(String(255), unique=True, nullable=True)
     plan_type: Mapped[PlanType] = mapped_column(Enum(PlanType, name="PlanType"), default=PlanType.FREE)
-    credits_remaining : Mapped[int] = mapped_column(default=2)
+    credits_remaining : Mapped[int] = mapped_column(default=200)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=func.now(), 
         onupdate=func.now())
